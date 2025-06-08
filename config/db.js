9272f99aa302652881e9882527c3f21ade943fb9
TsheringPemo <pemotk183@gmail.com>
2025-06-08 20:33:36 +0600

    Personal 1

--- config/db.js
+++ config/db.js
@@ -15,9 +15,9 @@ const db = pgp({
   database: process.env.DB_NAME || 'userAuth',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASS || 'your_password_here',
-  ssl: {
-    rejectUnauthorized: false // set to true if you want to enforce ssl certificate validation
-  }
+  //ssl: {
+    //rejectUnauthorized: false // set to true if you want to enforce ssl certificate validation
+  //}
   });
 
 // Log connection success (optional)
